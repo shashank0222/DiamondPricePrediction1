d71638209f349d0224b097ca8c17ce0059ea64dc
shashank0222 <goelshashank58@gmail.com>
2023-08-15 05:49:59 +0530

    Data ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,59 @@
+import os
+import sys
+from src.logger import logging
+from src.exception import CustomException
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+## initialize the data ingestion configuration
+
+from dataclasses import dataclass
+
+@dataclass  # it's like a placeholder which itself create a class and we need not to write init method in class.
+class DataIngestionconfig :
+    train_data_path : str = os.path.join('artifacts','train.csv')
+    test_data_path : str = os.path.join('artifacts', 'test.csv')
+    raw_data_path:str = os.path.join('artifacts','raw.csv')
+
+
+## create the data ingestion class
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionconfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Data ingestion method starts")
+
+        try:
+            df = pd.read_csv(os.path.join('notebooks/data','gemstone.csv'))
+            logging.info('Dataset read as pandas dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path , index=False)
+
+            logging.info("Raw data is created")
+
+            train_set , test_set = train_test_split(df , test_size=0.30 , random_state=42)
+
+
+            # saving the dataset into train.csv and test.csv
+            train_set.to_csv(self.ingestion_config.train_data_path , index = False , header = True)
+            test_set.to_csv(self.ingestion_config.test_data_path , index = False , header = True)
+
+            logging.info('Ingestion of data is completed')
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+
+
+        except Exception as e:
+            logging.info("Exception occured at data ingestion stage")
+            raise CustomException(e,sys)
+
+
+
+
